5038f952a34a5dc340b9c325f8de475ac326344a
v7eZ3t <v7eZ3t@gmail.com>
2021-03-16 23:03:29 +0100

    rename

--- Main/Board.py
+++ Main/Board.py
@@ -8,4 +8,9 @@ for i in range(int(HORIZONTAL_TILES_NUMBER)):
     for j in range(int(VERTICAL_TILES_NUMBER)):
         board[i].append(random.randint(0, 3))
 
+# 0 - pole na medal
+# 1 - do zasiania
+# 2 - do podlania
+# 3 - do ścięcia
+
 print(board)
