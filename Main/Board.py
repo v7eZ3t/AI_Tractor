a2be4d3e9abf5a471f1bfd0aaf387b1340311662
Łukasz <lukkoz2@st.amu.edu.pl>
2021-03-16 23:07:34 +0100

    plansza update - przerobiona na metodę

--- Main/Board.py
+++ Main/Board.py
@@ -3,9 +3,9 @@ from Main.constants import *
 
 board = []
 
-for i in range(int(HORIZONTAL_TILES_NUMBER)):
-    board.append([random.randint(0, 3)])
-    for j in range(int(VERTICAL_TILES_NUMBER)):
-        board[i].append(random.randint(0, 3))
-
-print(board)
+def generate():
+    for i in range(int(HORIZONTAL_TILES_NUMBER)):
+        board.append([random.randint(0, 3)])
+        for j in range(int(VERTICAL_TILES_NUMBER)):
+            board[i].append(random.randint(0, 3))
+    return board
