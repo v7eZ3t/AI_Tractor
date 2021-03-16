f3211bc50fe25917170d90f3e9d4c0ff12ed3642
v7eZ3t <v7eZ3t@gmail.com>
2021-03-16 22:53:27 +0100

    rename

--- Main/Board.py
+++ Main/Board.py
@@ -3,9 +3,9 @@ from Main.constants import *
 
 board = []
 
-for i in range(HORIZONTAL_TILES_NUMBER):
+for i in range(int(HORIZONTAL_TILES_NUMBER)):
     board.append([random.randint(0, 3)])
-    for j in range(VERTICAL_TILES_NUMBER):
+    for j in range(int(VERTICAL_TILES_NUMBER)):
         board[i].append(random.randint(0, 3))
 
 print(board)
